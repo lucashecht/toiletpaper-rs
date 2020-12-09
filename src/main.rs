f2f53aae783cc28a579c1ffb31eef4bb117f0dfb
Lucas Hecht <lucas.hecht@agdsn.de>
2020-12-09 23:47:37 +0100

    Sum stock levels

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,25 @@ struct Availabilities {
     storeAvailabilities: HashMap<i32, Vec<ItemAttributes>>,
 }
 
+fn sum_stocklevels(items: HashMap<i32, Vec<ItemAttributes>>) -> i32 {
+    let mut sum: i32 = 0;
+
+    for (_item, attributes) in &items {
+        sum += attributes[0].stockLevel;
+    }
+
+    sum
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    
     let url = "https://products.dm.de/store-availability/DE/availability?dans=28171,45784&storeNumbers=2787";
     
     let res: Availabilities = reqwest::get(url).await?.json().await?;
 
-    println!("{:?}", res);
-    println!("{:?}", res.storeAvailabilities.get(&28171).unwrap()[0].stockLevel);
+    let stocklevel = sum_stocklevels(res.storeAvailabilities);
+
+    println!("Stock level: {}", stocklevel);
     Ok(())
 }
