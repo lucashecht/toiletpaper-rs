3b951b25191b52b1e1f17b55e5d98fb7940fa88f
Lucas Hecht <lucas.hecht@agdsn.de>
2020-12-10 00:59:46 +0100

    Add more product IDs

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,19 @@ fn sum_stocklevels(items: HashMap<i32, Vec<ItemAttributes>>) -> i32 {
     sum
 }
 
-
+fn build_url(store_id: String, item_ids: Vec<&str>) -> String { 
+    let url = format!("https://products.dm.de/store-availability/DE/availability?dans={items}&storeNumbers={store}",
+                      items = item_ids.join(","),
+                      store = store_id);
+    
+    url
+}
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let url = "https://products.dm.de/store-availability/DE/availability?dans=28171,45784&storeNumbers=2787";
-    
-    let res: Availabilities = reqwest::get(url).await?.json().await?;
+    let items = vec!["485698", "709006", "452744", "137425", "28171", "595420", "799358", "863567", "610544", "452740", "846857", "452753", "525943", "879536", "485695", "853483", "594080", "504606", "593761", "535981", "842480", "524535", "127048", "524533", "524532", "846834", "708997", "711915"];
+
+    let url = build_url("2787".to_string(), items);
+    let res: Availabilities = reqwest::get(&url).await?.json().await?;
 
     let stocklevel = sum_stocklevels(res.storeAvailabilities);
 
