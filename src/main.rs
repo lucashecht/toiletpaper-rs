544970e1bb3bb9a1af6620f3c5e62209821ee732
Lucas Hecht <lucas.hecht@agdsn.de>
2021-01-11 00:41:11 +0100

    Add Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 use toiletpaper::{establish_connection, save_stock};
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -9,10 +11,17 @@ struct ItemAttributes {
     stockLevel: i32,
 }
 
+#[macro_use] extern crate rocket;
+
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize)]
 struct Availabilities {
     storeAvailabilities: HashMap<i32, Vec<ItemAttributes>>,
+}
+
+#[get("/")]
+async fn stock() -> String {
+    get_toiletpaper_stock().await.to_string()
 }
 
 fn sum_stocklevels(items: HashMap<i32, Vec<ItemAttributes>>) -> i32 {
@@ -33,25 +42,41 @@ fn build_url(store_number: i32, item_ids: Vec<&str>) -> String {
     url
 }
 
-async fn get_toiletpaper_stock() -> Result<i32, reqwest::Error> {
+async fn get_stock(store_number: i32, item_ids: Vec<&str>) -> i32 {
+    let url = build_url(store_number, item_ids);
+    let response = match reqwest::get(&url).await {
+        Ok(resp) => resp,
+        Err(_) => {
+            println!("Error fetching stock");
+            return 0
+        }
+    };
+
+    match response.json::<Availabilities>().await {
+        Ok(result) => sum_stocklevels(result.storeAvailabilities),
+        Err(_) => {
+            println!("Error fetching stock");
+            0
+        }
+    }
+}
+
+async fn get_toiletpaper_stock() -> i32 {
     let store_number = 2787;
     let items = vec!["485698", "709006", "452744", "137425", "28171", "595420", "799358", "863567", "610544", "452740", "846857", "452753", "525943", "879536", "485695", "853483", "594080", "504606", "593761", "535981", "842480", "524535", "127048", "524533", "524532", "846834", "708997", "711915"];
 
-    let url = build_url(store_number, items);
-    let res: Availabilities = reqwest::get(&url).await?.json().await?;
-
-    Ok(sum_stocklevels(res.storeAvailabilities))
+    get_stock(store_number, items).await
 }
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+#[rocket::main]
+async fn main() {
     
-    let stocklevel = get_toiletpaper_stock().await?;
+    let stocklevel = get_toiletpaper_stock().await;
     println!("Stock level: {}", stocklevel);
 
     /*let connection = establish_connection();
     let product_type = String::from("toiletpaper");
     save_stock(&connection, &product_type, &stocklevel, &store_number);*/
 
-    Ok(())
+    rocket::ignite().mount("/", routes![stock]).launch().await;
 }
